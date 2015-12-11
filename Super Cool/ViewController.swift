c01e990caa9733e4322bd8fe07df8202d74443a4
Jason Richey <Jason@jasons-mbp.home>
2015-12-11 01:03:07 -0800

    Initial Commit

---
+++ Super Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool
+//
+//  Created by Jason Richey on 12/11/15.
+//  Copyright © 2015 Big Snout Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
